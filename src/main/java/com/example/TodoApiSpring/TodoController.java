22843ed6ce54172c2f78e6f6665f06727b443af7
mohitcod141 <mohitkmr3621@gmail.com>
2025-04-10 16:19:31 +0530

    Adding more controllers

--- src/main/java/com/example/TodoApiSpring/TodoController.java
+++ src/main/java/com/example/TodoApiSpring/TodoController.java
@@ -1,9 +1,14 @@
 package com.example.TodoApiSpring;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.List;
 
-
+@RestController
 public  class TodoController {
 
 
@@ -17,6 +22,30 @@ public  class TodoController {
         todos.add(new Todo(3 , true , "Todo 3"  ,3));
 
     }
+
+    @GetMapping("/Todos")
+    public List<Todo> getTodos(){
+        return todos;
+    }
+
+    @PostMapping("/Todos")
+    public Todo createTodo(@RequestBody Todo newTodo){
+
+        todos.add(newTodo);
+        return newTodo;
+
+
+    }
+    @PostMapping("/AddMultipleTodo")
+    public ArrayList<Todo> createMultipleTodo(@RequestBody ArrayList <Todo> newTodos){
+        todos.addAll(newTodos);
+        return newTodos;
+    }
+
+
+
+
+
     }
 
 
